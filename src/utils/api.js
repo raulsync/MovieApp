9453df41ef8fbabbf1258896fc3e60ded7c2978f
Rahul Anand <duffer.rdx@gmail.com>
2024-05-22 00:07:09 +0530

    api_key update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,25 +1,25 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const BASE_URL = 'https://api.themoviedb.org/3'
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 // console.log("TMDB_TOKEN", TMDB_TOKEN);
 
 const headers = {
   Authorization: 'bearer ' + TMDB_TOKEN,
-}
+};
 
 export const fetchDataFromApi = async (url, params) => {
   try {
     const { data } = await axios.get(BASE_URL + url, {
       headers,
       params,
-    })
-    // console.log("Request URL:", BASE_URL + url);
+    });
+    console.log('Request URL:', BASE_URL + url);
 
-    return data
+    return data;
   } catch (err) {
-    console.log(err)
-    return err
+    console.log(err);
+    return err;
   }
-}
+};
